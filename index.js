40bded438cbc56ac77b99a532bd0c43b058c9f80
Paul Wafula <paulwafula391@gmail.com>
2025-04-21 12:24:45 +0300

    arraysmaps

--- index.js
+++ index.js
@@ -12,5 +12,14 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
-}
+  return tutorials.map(tutorial => {
+    const words = tutorial.split(" ");
+    const capitalizedWords = words.map(word =>
+      word.charAt(0).toUpperCase() + word.slice(1) // preserve existing casing
+    );
+    return capitalizedWords.join(" ");
+  });
+};
+
+
+
